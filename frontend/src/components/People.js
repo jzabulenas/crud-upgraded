f720b83ab40dbfa3fb95d690e45098df509b97e9
Julius <jzabulenas@protonmail.com>
2023-09-07 20:52:49 +0300

    Display people in a mobile friendly table

--- frontend/src/components/People.js
+++ frontend/src/components/People.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import "../style.scss";
+import "./People.scss";
 
 export default function People() {
     const [people, setPeople] = useState([]);
@@ -24,13 +25,39 @@ export default function People() {
 
     return (
         <article className='container'>
-            {people.map(person => {
-                return (
-                    <>
-                        <p>{person.firstName} {person.lastName}</p>
-                    </>
-                )
-            })}
-        </article>
+            <table className='table responsive'>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>First name</th>
+                        <th>Last name</th>
+                        <th>Birthday</th>
+                        <th>Address</th>
+                        <th>City</th>
+                        <th>State</th>
+                        <th>Zip code</th>
+                        <th>Phone number</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {people.map(person => {
+                        return (
+                            <tr key={person.id}>
+                                <td data-label="id">{person.id}</td>
+                                <td data-label="First name">{person.firstName}</td>
+                                <td data-label="Last name">{person.lastName}</td>
+                                <td data-label="Birthday">{person.birthday}</td>
+                                <td data-label="Address">{person.address}</td>
+                                <td data-label="City">{person.city}</td>
+                                <td data-label="State">{person.state}</td>
+                                <td data-label="Zip code">{person.zipCode}</td>
+                                <td data-label="Phone number">{person.phoneNumber}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </article >
     );
 }
